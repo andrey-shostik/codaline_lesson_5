74e894337523a9227364e4b25d5859932b1b2ae4
andrey9828 <aaa73731919@gmail.com>
2016-01-11 01:28:04 +0200

    added bootstrap, update all views

--- Gemfile
+++ Gemfile
@@ -23,8 +23,8 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
-
-
+gem 'bootstrap-sass', '~> 3.3.6'
+gem 'autoprefixer-rails'
 
 # Use Unicorn as the app server
 # gem 'unicorn'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	
+
 	def show
 		@post = Post.where(id: params[:id]).first
 	end
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
-
+    
   def new
 		@post = Post.new
   end
